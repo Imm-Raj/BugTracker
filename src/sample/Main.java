d52126043a914c4708551aab60a8c142bf234750
Arun27 <immanuelraj154@gmail.com>
2021-08-28 19:40:03 +0100

    Second commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,47 +10,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-// public class Main extends Application {
-//
-//
-//
-//    private Scene scene;
-//
-//
-//    @Override
-//    public void start(Stage primaryStage) throws Exception{
-//
-//
-//        LoginOrSignUpGUI ls = new LoginOrSignUpGUI();
-//
-//        scene = new Scene(ls.getLSPage(), 1430,730);
-//
-//        primaryStage.setTitle("TraxErr");
-//
-//        primaryStage.setScene(scene);
-//
-//        ls.addStage(primaryStage);
-//
-//         //JavaFX must have a Scene (window content) inside a Stage (window)
-//
-//        createAndFillAccounts();
-//
-//
-//        System.out.println(primaryStage);
-//
-//        System.out.println(scene.toString());
-//
-//        //Show the Stage (window)
-//        primaryStage.show();
-//
-//
-//    }
-
-public class Main extends Application {
+ public class Main extends Application {
 
 
-
-   private static AllCreatedAccounts aca;
 
     private Scene scene;
 
@@ -58,47 +20,25 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        VBox mainBox = new VBox();
 
-        createAndFillAccounts();
+        LoginOrSignUpGUI ls = new LoginOrSignUpGUI();
 
-        AllCreatedProjects allCreatedProjects = AllCreatedProjects.getInstance();
-
-        Account mainAcc = aca.findAccountUsingUserName("Julian123");
-
-        allCreatedProjects.addProject("TraxErr", "This is cool", mainAcc, aca.getAccountsList() );
-
-        allCreatedProjects.getProjectFromList("0").createTicketForThisProject("First Ticket", "This is a very first ticket" +
-                "", 1,1, mainAcc, aca.getAccountsList());
-
-        allCreatedProjects.getProjectFromList("0").printProjectInfo();
-
-        Ticket t = allCreatedProjects.getProjectFromList("0").getTicketUsingID("0 - 0");
-
-        DisplayCompTicket displayCompTicket = new DisplayCompTicket(t);
-
-        List<Closable> compList = new ArrayList<>();
-
-        compList.add(displayCompTicket);
-
-        SearchableList searchableList = new SearchableList(compList, "Tickets", 500,500);
-
-
-        mainBox.getChildren().add(searchableList.getMainPane());
-
-        scene = new Scene(mainBox, 1430,730);
+        scene = new Scene(ls.getLSPage(), 1430,730);
 
         primaryStage.setTitle("TraxErr");
 
         primaryStage.setScene(scene);
 
+        ls.addStage(primaryStage);
+
+         //JavaFX must have a Scene (window content) inside a Stage (window)
+
+        createAndFillAccounts();
 
 
-        //JavaFX must have a Scene (window content) inside a Stage (window)
+        System.out.println(primaryStage);
 
-
-
-
+        System.out.println(scene.toString());
 
         //Show the Stage (window)
         primaryStage.show();
@@ -106,13 +46,74 @@ public class Main extends Application {
 
     }
 
+//
+//public class Main extends Application {
+//
+//
+//
+//   private static AllCreatedAccounts aca;
+//
+//    private Scene scene;
+//
+//
+//    @Override
+//    public void start(Stage primaryStage) throws Exception{
+//
+//        VBox mainBox = new VBox();
+//
+//        createAndFillAccounts();
+//
+//        AllCreatedProjects allCreatedProjects = AllCreatedProjects.getInstance();
+//
+//        Account mainAcc = aca.findAccountUsingUserName("Julian123");
+//
+//        allCreatedProjects.addProject("TraxErr", "This is cool", mainAcc, aca.getAccountsList() );
+//
+//        allCreatedProjects.getProjectFromList("0").createTicketForThisProject("First Ticket", "This is a very first ticket" +
+//                "", 1,1, mainAcc, aca.getAccountsList());
+//
+//        allCreatedProjects.getProjectFromList("0").printProjectInfo();
+//
+//        Ticket t = allCreatedProjects.getProjectFromList("0").getTicketUsingID("0 - 0");
+//
+//        DisplayCompTicket displayCompTicket = new DisplayCompTicket(t);
+//
+//        List<Closable> compList = new ArrayList<>();
+//
+//        compList.add(displayCompTicket);
+//
+//        SearchableList searchableList = new SearchableList(compList, "Tickets", 500,500);
+//
+//
+//        mainBox.getChildren().add(searchableList.getMainPane());
+//
+//        scene = new Scene(mainBox, 1430,730);
+//
+//        primaryStage.setTitle("TraxErr");
+//
+//        primaryStage.setScene(scene);
+//
+//
+//
+//        //JavaFX must have a Scene (window content) inside a Stage (window)
+//
+//
+//
+//
+//
+//        //Show the Stage (window)
+//        primaryStage.show();
+//
+//
+//    }
+
     public static void main(String[] args) {
         launch(args);
     }
-
+//
     public static void createAndFillAccounts() {
 
-           aca = AllCreatedAccounts.getInstance();
+        AllCreatedAccounts   aca = AllCreatedAccounts.getInstance();
 
                         String[] selectionOne = {"Developer", "Tester"};
                         String[] selectionTwo = {"Analyst"};
